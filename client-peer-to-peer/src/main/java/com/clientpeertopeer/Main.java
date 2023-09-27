c7fe510be11a9aad62c9bcc4a7d778437c1e55c0
gusmgoes <ggoes269@gmail.com>
2023-09-27 12:01:03 -0400

    Adding feat of connection and file search in the project

--- client-peer-to-peer/src/main/java/com/clientpeertopeer/Main.java
+++ client-peer-to-peer/src/main/java/com/clientpeertopeer/Main.java
@@ -1,12 +1,18 @@
 package com.clientpeertopeer;
 
 import org.apache.commons.net.ftp.FTPFile;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
 import org.apache.commons.net.ftp.FTPClient;
 
 public class Main {
     public static void main(String[] args) {
-        String server = "localhost";
         int port = 21; // everything reminds me u
+        String server = "localhost";
         String user = "gusmgoes";
         String password = "1234";
 
@@ -14,18 +20,30 @@ public class Main {
 
         try {
             ftpClient.connect(server, port);
-            
             ftpClient.login(user, password);
+            System.out.println("Hey, I successfully logged in");
 
-            FTPFile[] files = ftpClient.listFiles();
-            for (FTPFile file : files) {
-                System.out.println(file.getName());
+            String remoteFilePath = "/teste.txt";
+
+            InputStream inputStream = ftpClient.retrieveFileStream(remoteFilePath);
+            if (inputStream != null) {
+                BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    System.out.println(line);
+                }
+                reader.close();
+                inputStream.close();
+            } else {
+                System.err.println("Unable to locate this file, path: " + remoteFilePath);
             }
 
             ftpClient.logout();
             ftpClient.disconnect();
+        } catch (IOException e) {
+            System.err.println("Error in I/O operation: " + e.getMessage());
         } catch (Exception e) {
-            System.err.println("Error connecting or listing files: " + e.getMessage());
+            System.err.println("Error: " + e.getMessage());
         }
     }
 }
--- peer-to-peer/src/main/java/com/peertopeer/Main.java
+++ peer-to-peer/src/main/java/com/peertopeer/Main.java
@@ -1,32 +1,55 @@
 package com.peertopeer;
 import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.apache.ftpserver.FtpServer;
 import org.apache.ftpserver.FtpServerFactory;
+import org.apache.ftpserver.ftplet.Authority;
+import org.apache.ftpserver.ftplet.UserManager;
 import org.apache.ftpserver.listener.ListenerFactory;
 import org.apache.ftpserver.usermanager.PropertiesUserManagerFactory;
+import org.apache.ftpserver.usermanager.impl.BaseUser;
+import org.apache.ftpserver.usermanager.impl.WritePermission;
 
 public class Main {
     public static void main(String[] args) {
-
         //https://mina.apache.org/ftpserver-project/embedding_ftpserver.html
+        //https://stackoverflow.com/questions/8969097/writing-a-java-ftp-server
         int port = 21; // everything reminds me u
 
-        FtpServerFactory serverFactory = new FtpServerFactory(); // Instantiate the FTP server factory
-        ListenerFactory factory = new ListenerFactory(); // Instantiate the listener factory
+        FtpServerFactory serverFactory = new FtpServerFactory(); // instantiate the FTP server factory
+        ListenerFactory factory = new ListenerFactory(); // instantiate the listener factory
              
-        factory.setPort(port); // Set the port 21 to the factory listener
+        factory.setPort(port); // set the port 21 to the server listen
 
-        serverFactory.addListener("default", factory.createListener()); // Replaces the default listener with the one we defined
+        serverFactory.addListener("default", factory.createListener()); // replaces the default listener with the one we defined
 
-        PropertiesUserManagerFactory userManagerFactory = new PropertiesUserManagerFactory(); // Instantiate the Properties of User factory
+        PropertiesUserManagerFactory userManagerFactory = new PropertiesUserManagerFactory(); // instantiate the Properties of User factory
 
-        userManagerFactory.setFile(new File("src//main//users.properties")); // Defines the file for us to configure
-        serverFactory.setUserManager(userManagerFactory.createUserManager()); // Set the userManagerFactory into the server FTP
+        userManagerFactory.setFile(new File("src//main//users.properties")); // defines the file for us to configure
+        UserManager userManager = userManagerFactory.createUserManager();
 
-        FtpServer server = serverFactory.createServer(); 
+        BaseUser user = new BaseUser(); // create a user with username = gusmgoes and password = 1234, this credentials is used in login the user in client ftp
+        user.setName("gusmgoes");
+        user.setPassword("1234");
+        user.setHomeDirectory("src//main//general"); // gives him access to the general folder and the files inside him
 
-        try {     
+        List<Authority> roles = new ArrayList<Authority>();
+        roles.add(new WritePermission()); // have permission to write in the files
+        user.setAuthorities(roles);
+
+        try {   
+            userManager.save(user);
+            System.out.println("Success in create a user");
+        } catch (Exception e) {
+            System.err.println("Error: " + e.getMessage());
+        }
+        serverFactory.setUserManager(userManager);
+
+        FtpServer server = serverFactory.createServer();
+
+        try {   
             server.start();  // really start the server
             System.out.println("FTP server started on port " + port);
         } catch (Exception e) {
